8ed0e1f642e75f230171405d38a8a90209716e53
Asif Ali <asif.a.7277@gmail.com>
2022-11-15 14:46:41 +0530

    size of ccopyright

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -39,7 +39,7 @@ export default function Footer() {
             <h1 class=" text-4xl text-transparent hover:text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 hover:text-white text-2xl lg:text-4xl leading-relaxed font-newton tracking-wider">
               TV KILLS
             </h1>
-            <p class=" text-2xl mt-6 text-transparent hover:text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 hover:text-white ">
+            <p class=" text-base mt-6 text-transparent hover:text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 hover:text-white ">
               <CopyrightIcon
                 style={{
                   color: "purple",
